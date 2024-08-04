0388627b0d2180a58c4f43fcc3d15136470fd85e
Bob Jacobsen <jacobsen@mac.com>
2024-08-03 18:26:33 -0700

    new file to ensure directory mode bit set for sharing

---
+++ setModes.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Set the modes on shared directories for use by the Dispatcher account
+
+chmod 755 Dispatcher.jmri
+chmod 755 Dispatcher.jmri/profile
+chmod 755 Dispatcher.jmri/profile/e2eee3fb-a592-43c2-843b-ac0ae819edb9
+chmod 755 Dispatcher.jmri/profile/f33fcc47-0182-467f-be88-d2a8dd29d5a7
+chmod 755 Dispatcher.jmri/signal
+chmod 755 Dispatcher.jmri/throttle
+
+chmod 755 Dispatcher_Simple.jmri
+chmod 755 Dispatcher_Simple.jmri/profile
+chmod 755 Dispatcher_Simple.jmri/profile/e2eee3fb-a592-43c2-843b-ac0ae819edb9
+chmod 755 Dispatcher_Simple.jmri/profile/f33fcc47-0182-467f-be88-d2a8dd29d5a7
+chmod 755 Dispatcher_Simple.jmri/signal
+chmod 755 Dispatcher_Simple.jmri/throttle
